841654523078bde97936b63d003c9a3c0a2873f4
v_brooke <92850830+vbrooke78@users.noreply.github.com>
2022-02-15 14:59:37 +0000

    updated SQL string on fetchArticlesById function

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 exports.fetchArticlesById = (id) => {
   return db
-    .query('SELECT * FROM articles WHERE article_id = $1', [id])
+    .query('SELECT * FROM articles WHERE article_id = $1;', [id])
     .then(({ rows }) => {
       const article = rows[0];
       if (!article) {

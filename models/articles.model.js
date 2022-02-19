8a2f0e54f25cec96945ea6151f130b175bed22d1
vbrooke78 <vscotter@hotmail.com>
2022-02-19 20:14:57 +0000

    this commit will move the checkArticleExists function to the util.function.js file and refactor code accordingly

--- models/articles.model.js
+++ models/articles.model.js
@@ -77,16 +77,3 @@ exports.fetchArticles = async (
 
   return rows;
 };
-
-exports.checkArticleExists = (articleId) => {
-  return db
-    .query('SELECT * FROM articles WHERE article_id = $1;', [articleId])
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: `No such article_id: ${articleId}`,
-        });
-      }
-    });
-};

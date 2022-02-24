98bc5ffd47085fa91f56fb21a6c926e9a51c97aa
vbrooke78 <vscotter@hotmail.com>
2022-02-24 21:34:56 +0000

    this commit will update the getApi endpoint

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,5 +2,6 @@ const api = require('../endpoints.json');
 
 exports.getApi = (req, res, next) => {
   const stringifiedApi = JSON.stringify(api);
-  res.status(200).send(JSON.stringify(stringifiedApi));
+  console.log(stringifiedApi);
+  res.status(200).send(stringifiedApi);
 };

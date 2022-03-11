53642ffda0ded21135c1b1538b80e6e0a4142b25
vbrooke78 <vscotter@hotmail.com>
2022-03-11 11:33:11 +0000

    this commit will refactor the code to make use of express routers

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 const express = require('express');
 const app = express();
-const { getTopics } = require('./controllers/topics.controller');
-const { getUsers } = require('./controllers/users.controller');
-const {
-  getArticlesById,
-  patchArticleById,
-  getArticles,
-} = require('./controllers/articles.controller');
-const {
-  getComments,
-  postComment,
-  deleteCommentByCommentId,
-} = require('./controllers/comments.controller');
+
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors,
 } = require('./controllers/errors.controller');
-const { getApi } = require('./controllers/api.controller');
+
+const apiRouter = require('./routes/api-router');
 
 app.use(express.json());
-app.get('/api/topics', getTopics);
-app.get('/api/articles/:article_id', getArticlesById);
-app.patch('/api/articles/:article_id', patchArticleById);
-app.get('/api/users', getUsers);
-app.get('/api/articles', getArticles);
-app.get('/api/articles/:article_id/comments', getComments);
-app.post('/api/articles/:article_id/comments', postComment);
-app.delete('/api/comments/:comment_id', deleteCommentByCommentId);
-app.get('/api', getApi);
+
+app.use('/api', apiRouter);
 
 //Error handling
 app.use(handleCustomErrors);
---
+++ routes/topics-router.js
@@ -0,0 +1,7 @@
+const { getTopics } = require('../controllers/topics.controller');
+
+const topicsRouter = require('express').Router();
+
+topicsRouter.get('/', getTopics);
+
+module.exports = topicsRouter;
